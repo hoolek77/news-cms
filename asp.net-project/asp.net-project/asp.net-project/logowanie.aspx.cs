faac065ea910e5f09f21f5b8172b699d54d66b2d
jvncziii <dan.porzucek@gmail.com>
2021-01-07 16:18:21 +0100

    Established connection to a database

--- asp.net-project/asp.net-project/asp.net-project/logowanie.aspx.cs
+++ asp.net-project/asp.net-project/asp.net-project/logowanie.aspx.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace asp.net_project
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText ="select * from admins";
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Response.Write(reader.GetString(reader.GetOrdinal("username")));
+            }
+            con.Close();
         }
     }
 }
