01ff88d9a628a850c3eee7da77c83afd9c9cc5d7
jvncziii <dan.porzucek@gmail.com>
2021-01-11 18:22:04 +0100

    fixing bad commit

--- asp.net-project/asp.net-project/asp.net-project/addPostNested.master.cs
+++ asp.net-project/asp.net-project/asp.net-project/addPostNested.master.cs
@@ -21,7 +21,7 @@ namespace asp.net_project
             con.Open();
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = System.Data.CommandType.Text;
-            cmd.CommandText = "INSERT INTO users (Author,Title,Content,Thumbnail,Published) VALUES('"+author.Value+ "','" + title.Value + "','" + postContent.Value + "','" + thumbnail.Value + "',GETDATE())";
+            cmd.CommandText = "INSERT INTO users (Author,Title,Content,Thumbnail,Published)VALUES('"+author.Value+ "','" + title.Value + "','" + postContent.Value + "','" + thumbnail.Value + "',GETDATE())";
             SqlDataReader reader = cmd.ExecuteReader();
             con.Close();
         }
