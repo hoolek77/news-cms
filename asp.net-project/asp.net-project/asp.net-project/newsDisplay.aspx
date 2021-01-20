aeabe03a46b4a382c296f438344d9fdfb7418abb
hoolek77 <szymon.kin@gmail.com>
2021-01-20 15:43:49 +0100

    created page for displaying a single news

---
+++ asp.net-project/asp.net-project/asp.net-project/newsDisplay.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/newsDisplayNested.master" AutoEventWireup="true" CodeBehind="newsDisplay.aspx.cs" Inherits="asp.net_project.WebForm9" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <style>
+        .title {
+            font-size: 3.6rem;
+        }
+
+        .author {
+            margin: 1rem;
+            font-size: 2.4rem;
+        }
+
+        .img {
+            height: 30rem;
+            width: auto;
+        }
+
+        .content {
+            width: 40%;
+            font-size: 2rem;
+            max-width: 100%;
+            word-wrap: break-word;
+            margin: 3rem 10rem;
+        }
+    </style>
+    <div class="title" id="title" runat="server"></div>
+    <div class="author" id="author" runat="server"></div>
+    <img class="img" id="img" runat="server" />
+    <p class="content" id="content" runat="server"></p>
+</asp:Content>
---
+++ asp.net-project/asp.net-project/asp.net-project/newsDisplay.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace asp.net_project
+{
+
+
+    public partial class WebForm9
+    {
+
+        /// <summary>
+        /// Kontrolka title.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl title;
+
+        /// <summary>
+        /// Kontrolka author.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl author;
+
+        /// <summary>
+        /// Kontrolka img.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage img;
+
+        /// <summary>
+        /// Kontrolka content.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl content;
+    }
+}
